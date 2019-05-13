c42bc05c69946ef69b0b964b2e9c6918e9fb0a23
Marc Collado <marc@collado.io>
2019-05-13 10:28:16 +0200

    Add Blog page query

--- src/utils/queries.js
+++ src/utils/queries.js
@@ -1,7 +1,24 @@
 import { graphql } from 'gatsby';
 
+// Blog page query
+export const queryBlogData = graphql`
+  fragment BlogData on MarkdownRemarkConnection {
+    edges {
+      node {
+        id
+        frontmatter {
+          title
+          date(formatString: "MMMM DD, YYYY")
+          path
+          excerpt
+        }
+      }
+    }
+  }
+`;
+
 // Work page query
-export const query = graphql`
+export const queryWorkData = graphql`
   fragment WorkData on MarkdownRemarkConnection {
     edges {
       node {
