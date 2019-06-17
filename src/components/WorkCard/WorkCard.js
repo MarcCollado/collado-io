f4c1eb52369f95fe198f5b869de267aad99e9595
Marc Collado <marc@collado.io>
2019-06-17 06:55:29 +0200

    Add titleTranslator to decode markdown title

--- src/components/WorkCard/WorkCard.js
+++ src/components/WorkCard/WorkCard.js
@@ -4,26 +4,33 @@ import { Link, StaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
 import styles from './WorkCard.module.css';
 
-const WorkCard = ({ data, excerpt, path, title }) => (
-  <div className={styles.card}>
-    <Link className={styles.card__link} to={path}>
-      <Img
-        fluid={data[title.toLowerCase()].childImageSharp.fluid}
-        alt={title}
-      />
-      <div className={styles.card__wrapper}>
-        <h2 className={styles.card__title}>{title}</h2>
-        <p className={styles.card__excerpt}>{excerpt}</p>
-      </div>
-    </Link>
-  </div>
-);
+// Returns the matching graphQL query name for each Frontmatter title
+const titleTranslator = {
+  iomando: 'iomando',
+  Ironhack: 'ironhack',
+  'Radio Lanza': 'radioLanza'
+};
+
+const WorkCard = ({ data, excerpt, path, title }) => {
+  const titleKey = titleTranslator[title];
+  return (
+    <div className={styles.card}>
+      <Link className={styles.card__link} to={path}>
+        <Img fluid={data[titleKey].childImageSharp.fluid} alt={title} />
+        <div className={styles.card__wrapper}>
+          <h2 className={styles.card__title}>{title}</h2>
+          <p className={styles.card__excerpt}>{excerpt}</p>
+        </div>
+      </Link>
+    </div>
+  );
+};
 
 export default (props) => (
   <StaticQuery
     query={graphql`
       query WorkCardQuery {
-        pansa: file(relativePath: { eq: "pansa.png" }) {
+        radioLanza: file(relativePath: { eq: "radio-lanza.png" }) {
           childImageSharp {
             fluid(maxWidth: 500) {
               ...GatsbyImageSharpFluid
@@ -54,7 +61,7 @@ WorkCard.propTypes = {
   data: PropTypes.shape({
     iomando: PropTypes.object.isRequired,
     ironhack: PropTypes.object.isRequired,
-    pansa: PropTypes.object.isRequired
+    radioLanza: PropTypes.object.isRequired
   }).isRequired,
   excerpt: PropTypes.string.isRequired,
   path: PropTypes.string.isRequired,
