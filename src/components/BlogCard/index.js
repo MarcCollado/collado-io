40d70d833528f1d08b6cef3c0e4b996fcbfd7dc4
Marc Collado <marc@collado.io>
2019-06-23 10:49:28 +0200

    Add TODO

--- src/components/BlogCard/index.js
+++ src/components/BlogCard/index.js
@@ -5,4 +5,5 @@ export { default as BlogCard } from './BlogCard';
 - Receives { title, date, path, excerpt } as props.
 - Links to { path } which renders the blog post through the BlogPost component.
 - TODO: { title, date, excerpt } could be passed down as props to BlogPost through state (location.state) within the Link component. But the data doesn't persist on BlogPost upon refresh.
+- TODO: show tags alongside the published date.
 */
