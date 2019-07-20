6c3e2267ac8e0d30748ccd10a1a9f97a47bdd9d1
Marc Collado <marc@collado.io>
2019-07-20 15:23:22 +0200

    Fix minor syntax, grammar, and formatting issues

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3,7 +3,7 @@ import { BlogCard } from '../components/BlogCard';
 import { EpisodeCard } from '../components/EpisodeCard';
 
 /*
-- Generates a tag-filtered array of BlogCard
+- Generates a tag-filtered array of BlogCards
 @ Params:
   - data: object returned from the allMarkdownRemark GraphQL query
   - tag: string with the tag filter
@@ -23,6 +23,34 @@ export function renderFilteredBlogCards(data, tag) {
       />
     ));
 }
+
+/*
+- Generates an array of BlogCards
+@ Params:
+  - data: object returned from the allMarkdownRemark GraphQL query
+@ Returns: array of BlogCards
+ */
+
+export function renderAllBlogCards(data) {
+  return data
+    .filter((edge) => !!edge.node.frontmatter.date)
+    .map((edge) => (
+      <BlogCard
+        key={edge.node.id}
+        title={edge.node.frontmatter.title}
+        date={edge.node.frontmatter.date}
+        path={edge.node.frontmatter.path}
+        excerpt={edge.node.frontmatter.excerpt}
+      />
+    ));
+}
+
+/*
+- Generates an array of EpisodeCards
+@ Params:
+  - data: object returned from the allMarkdownRemark GraphQL query
+@ Returns: array of EpisodeCards
+ */
 
 export function renderAllEpisodeCards(data) {
   return data
