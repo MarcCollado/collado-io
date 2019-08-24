ec02ed8ded91f734fc7e31c1c7362c1ac6132985
Marc Collado <marc@collado.io>
2019-08-24 09:42:25 +0200

    Update eslint config to be a JS file

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  plugins: ['html', 'jsx-a11y', 'react'],
+  rules: {
+    'no-console': 'warn',
+    'no-unused-vars': 'warn',
+    'no-useless-constructor': 'warn',
+    'jsx-a11y/anchor-is-valid': [
+      'warn',
+      {
+        specialLink: ['to']
+      }
+    ],
+    'react/forbid-prop-types': 'warn',
+    'react/jsx-filename-extension': 'off',
+    'react/prefer-stateless-function': 'warn',
+    'react/prop-types': 'warn',
+    'react/react-in-jsx-scope': 'off'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+};
