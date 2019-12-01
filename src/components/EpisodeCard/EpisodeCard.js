85e40404f16df51018d3749be1ca0a8d4d2552ab
Marc Collado <marc@collado.io>
2019-12-01 06:44:03 +0100

    Render episode in the episode title

--- src/components/EpisodeCard/EpisodeCard.js
+++ src/components/EpisodeCard/EpisodeCard.js
@@ -4,10 +4,12 @@ import { Link } from 'gatsby';
 import styles from './EpisodeCard.module.css';
 import { PublishedAt } from '../PublishedAt';
 
-const EpisodeCard = ({ path, title, excerpt, date }) => (
+const EpisodeCard = ({ path, title, excerpt, date, episode }) => (
   <div className={styles.card}>
     <Link className={styles.card__link} to={path}>
-      <h2 className={styles.card__title}>{title}</h2>
+      <h2 className={styles.card__title}>
+        {!Number.isNaN(parseInt(episode, 10)) ? `${episode}: ${title}` : title}
+      </h2>
       <p className={styles.card__excerpt}>{excerpt}</p>
       <PublishedAt date={date} />
     </Link>
@@ -18,7 +20,8 @@ EpisodeCard.propTypes = {
   date: PropTypes.string.isRequired,
   excerpt: PropTypes.string,
   path: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
+  episode: PropTypes.string.isRequired
 };
 
 EpisodeCard.defaultProps = {
