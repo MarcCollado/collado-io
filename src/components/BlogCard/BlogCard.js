289b42ffd7b4bf640747a8913d39af3f0c036906
Marc Collado <marc@collado.io>
2020-03-10 10:03:11 +0100

    Render card dynamically depending on props

--- src/components/BlogCard/BlogCard.js
+++ src/components/BlogCard/BlogCard.js
@@ -1,27 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
+import Img from 'gatsby-image';
 import styles from './BlogCard.module.css';
 import { PublishedAt } from '../PublishedAt';
 // TODO: { title, date, excerpt } (even html and the rest of BlogPost params could be requested from the query) could be passed down as props to BlogPost through state (location.state) within the Link component. But the data doesn't persist on BlogPost upon refresh.
-const BlogCard = ({ title, date, path, excerpt }) => (
-  <div className={styles.card}>
-    <Link className={styles.card__link} to={path}>
-      <h2 className={styles.card__title}>{title}</h2>
-      <p className={styles.card__excerpt}>{excerpt}</p>
-      <PublishedAt date={date} />
-    </Link>
-  </div>
+const BlogCard = ({ image, title, date, path, excerpt }) => (
+  <Link className={styles.card__link} to={path}>
+    <div className={styles.card__container}>
+      {image && (
+        <div className={styles.card__image}>
+          <Img fluid={image} alt={title} />
+        </div>
+      )}
+      <div className={styles.card__content}>
+        <h2 className={styles.card__title}>{title}</h2>
+        <p className={styles.card__excerpt}>{excerpt}</p>
+        {date && <PublishedAt date={date} />}
+      </div>
+    </div>
+  </Link>
 );
 
 BlogCard.propTypes = {
+  image: PropTypes.string,
   title: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired,
+  date: PropTypes.string,
   path: PropTypes.string.isRequired,
   excerpt: PropTypes.string
 };
 
 BlogCard.defaultProps = {
+  image: '',
+  date: '',
   excerpt: ''
 };
 
