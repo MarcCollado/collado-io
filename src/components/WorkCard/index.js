dd2a3b5f3db23b21df611bfdd79c86d505b786e1
Marc Collado <marc@collado.io>
2020-04-12 09:50:10 +0200

    Deprecate WorkCard

--- src/components/WorkCard/index.js
+++
@@ -1,9 +0,0 @@
-export { default as WorkCard } from './WorkCard';
-
-/* WorkCard.js
-- Renders a card for any given work post.
-- Receives { title, path, excerpt } as props.
-- Fetches thumbnail image through <StaticQuery /> for each work post.
-- Unlike BlogCard, { path } links to the custom work page instead of the template page (BlogPost).
-- TODO: { title, excerpt } could be passed down as props to the work page through state (location.state) within the Link component. But the data doesn't persist upon refresh.
-*/
