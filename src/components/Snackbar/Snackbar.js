0b6ca59322561cbdd717099b56f5b28d8dd7dda2
Marc Collado <marc@collado.io>
2020-12-13 10:57:26 +0100

    Update snackbar

--- src/components/Snackbar/Snackbar.js
+++ src/components/Snackbar/Snackbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import styles from './Snackbar.module.css';
 
 const Snackbar = ({ unmount }) => (
   <div className={styles.snackbar__container}>
     <p className={styles.snackbar__text}>
-      {`🎧 Jimmy Flores and I just launched a new podcast:`}{' '}
-      <a href="/work/radio-lanza"> say hi to Radio Lanza</a>
+      {`🎙 Ramon Gilabert and I just launched a new podcast —`}{' '}
+      <Link to="/blog/2020/safareig"> say hi to Safareig</Link>
     </p>
     <button onClick={unmount} className={styles.snackbar__close}>
       <span>✕</span>
