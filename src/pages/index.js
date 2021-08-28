ab1af3ea94beec8ff4c6e53a117233ad59d5b4bb
Marc Collado <marc@collado.io>
2021-08-28 19:40:37 +0200

    Refactor home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import Img from 'gatsby-image';
 import PropTypes from 'prop-types';
 import { Layout } from '../components/Layout';
 import { Header } from '../components/Header';
 
 const HomePage = ({ data, location }) => {
-  const homePageImg = data.homePageImg.childImageSharp.fluid;
   const homePageInfo = {
     title: data.homePageInfo.edges[0].node.frontmatter.title,
     excerpt: data.homePageInfo.edges[0].node.frontmatter.excerpt,
@@ -15,12 +13,6 @@ const HomePage = ({ data, location }) => {
   return (
     <Layout title="Home" description="" pathname={location.pathname}>
       <Header title={homePageInfo.title} tagline={homePageInfo.excerpt} />
-      <Img
-        title={homePageInfo.title}
-        alt={homePageInfo.title}
-        fluid={homePageImg}
-        style={{ width: `15em`, margin: `0 auto -1em` }}
-      />
       <div dangerouslySetInnerHTML={{ __html: homePageInfo.html }} />
     </Layout>
   );
@@ -28,13 +20,6 @@ const HomePage = ({ data, location }) => {
 
 export const query = graphql`
   {
-    homePageImg: file(relativePath: { eq: "pages/marc.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 800) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
     homePageInfo: allMarkdownRemark(
       filter: { fileAbsolutePath: { regex: "/src/content/md/pages/home.md/" } }
       limit: 1
@@ -46,7 +31,6 @@ export const query = graphql`
 
 HomePage.propTypes = {
   data: PropTypes.shape({
-    homePageImg: PropTypes.object,
     homePageInfo: PropTypes.shape({
       edges: PropTypes.arrayOf(
         PropTypes.shape({
