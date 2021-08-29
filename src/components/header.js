e96834266784ed55a29dfce82fb28200dac7b687
Marc Collado <marc@collado.io>
2021-08-29 09:14:51 +0200

    Refactor header

---
+++ src/components/header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// Styled Components
+
+const FlexCenter = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Title = styled.h1`
+  margin: 0 auto;
+`;
+
+const Subtitle = styled.p`
+  margin-block-start: 0.25em;
+  font-size: smaller;
+  text-transform: uppercase;
+  color: var(--fakeAsbestos88);
+`;
+
+// Main Components
+
+const Header = ({ subtitle, title }) => (
+  <FlexCenter>
+    <Title>{title}</Title>
+    <Subtitle>{subtitle}</Subtitle>
+  </FlexCenter>
+);
+
+export default Header;
