f57a3fc5852137333e21d8456ddbe2a6466c0797
Marc Collado <marc@collado.io>
2021-09-11 18:13:43 +0200

    Add renderPageLayout

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Header from '../components/header';
+import Layout from '../components/layout';
 import PostCard from '../components/postCard';
 
 // Dictionary used by work.js to translate k/v pairs
@@ -66,6 +68,26 @@ export function renderPosts(edges, filterTag = '') {
       />
     ));
 }
+
+/**
+ * Generates and renders a page layout
+ * @generator
+ * @param {object} md - data from a md page
+ * @param {string} pathname — location.pathname page prop
+ * @returns {object} - jsx for the layout + header
+ */
+export const renderPageLayout = (md, pathname) => (
+  <Layout
+    article={false}
+    description={md.excerpt}
+    image={null}
+    pathname={pathname}
+    title={md.title}
+  >
+    <Header title={md.title} subtitle={md.excerpt} />
+    <div dangerouslySetInnerHTML={{ __html: md.html }} />
+  </Layout>
+);
 
 /**
  * Currently deprecated
