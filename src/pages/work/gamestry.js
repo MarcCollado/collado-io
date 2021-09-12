3e0b4f249dd0737e279c95627332cfadfdded913
Marc Collado <marc@collado.io>
2021-09-12 11:17:32 +0200

    Use RenderLayout

--- src/pages/work/gamestry.js
+++ src/pages/work/gamestry.js
@@ -1,46 +1,36 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import { GatsbyImage } from 'gatsby-plugin-image';
 
-// Components
-import Header from '../../components/header';
-import Layout from '../../components/layout';
+import RenderPage from '../../components/renderPage';
 
-// Utils
-import * as styles from './work.module.css';
 import { renderPosts, extractMarkdown } from '../../utils/helpers';
 
 const Gamestry = ({ data, location }) => {
   const coverImg = data.coverImg.childImageSharp.gatsbyImageData;
   const seoImg = data.coverImg.childImageSharp.gatsbyImageData.src;
-  const pageInfo = extractMarkdown(data.pageInfo.edges);
+  const md = extractMarkdown(data.md.edges);
   const posts = data.posts.edges;
+
   return (
-    <Layout
-      title={pageInfo.title}
-      description={pageInfo.excerpt}
+    <RenderPage
+      article={false}
+      coverImage={coverImg}
+      md={md}
       pathname={location.pathname}
-      image={seoImg}
+      seoImage={seoImg}
     >
-      <Header title={pageInfo.title} subtitle={pageInfo.excerpt} />
-      <GatsbyImage
-        image={coverImg}
-        className={styles.image}
-        title={pageInfo.title}
-        alt={pageInfo.excerpt}
-      />
-      <div dangerouslySetInnerHTML={{ __html: pageInfo.html }} />
       {renderPosts(posts)}
-    </Layout>
+    </RenderPage>
   );
 };
 
 export const query = graphql`
   {
-    pageInfo: allMarkdownRemark(
+    md: allMarkdownRemark(
       filter: {
         fileAbsolutePath: { regex: "/src/content/md/pages/gamestry.md/" }
       }
+      limit: 1
     ) {
       ...pageMarkdown
     }
