2c4543c23728c1cca830e0566ac73345fd27eab7
Marc Collado <marc@collado.io>
2021-09-18 18:37:40 +0200

    Use fluid

--- src/components/layoutt.js
+++ src/components/layoutt.js
@@ -4,54 +4,36 @@ import { Link, useStaticQuery, graphql } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
 import 'normalize.css';
 
+import { fluid } from '../utils/fluid';
 import Header from './header';
 import { FlexCenter } from '../styles/containers';
+import GlobalStyles from '../styles/global';
 import Seo from '../utils/seo';
 
 // Styled Components
 
 // TODO: get rid of this container by making outter flex
-const InnerContainer = styled.div`
-  margin: auto 2em;
+const Container = styled.div`
   display: flex;
   flex-flow: column;
-
-  @media (min-width: 576px) {
-    margin: auto 3em;
-  }
-
-  @media (min-width: 768px) {
-    margin: auto 4em;
-  }
-
-  @media (min-width: 1024px) {
-    margin: auto 5em;
-  }
+  margin-block-end: ${fluid(16, 32)};
 `;
 
 const StyledAvatar = styled(GatsbyImage)`
   width: 4em;
-  margin: 0em auto 1.5em;
+  margin: 0em auto ${fluid(24, 36)};
 
   & img {
     border-radius: 2em;
   }
 
-  @media (min-width: 576px) {
-    margin: 0em auto 2em;
-  }
-
   @media (min-width: 768px) {
-    margin: 0em auto 2.5em;
     transition: all 300ms ease-in-out;
 
     &:hover {
       transform: ${(props) =>
         props.pathname === '/' ? 'scale(1, 1)' : 'scale(1.15, 1.15)'};
     }
-  }
-  @media (min-width: 1024px) {
-    margin: 0em auto 3em;
   }
 `;
 
@@ -78,6 +60,7 @@ const Layoutt = ({ children, article, coverImage, md, pathname, seoImage }) => {
 
   return (
     <>
+      <GlobalStyles />
       <Seo
         isArticle={article}
         pageDescription={excerpt}
@@ -85,7 +68,7 @@ const Layoutt = ({ children, article, coverImage, md, pathname, seoImage }) => {
         pageTitle={title}
         pathname={pathname}
       />
-      <InnerContainer>
+      <Container>
         <FlexCenter>
           <Link to="/">
             <StyledAvatar
@@ -101,7 +84,7 @@ const Layoutt = ({ children, article, coverImage, md, pathname, seoImage }) => {
         )}
         {!article && <div dangerouslySetInnerHTML={{ __html: html }} />}
         {children}
-      </InnerContainer>
+      </Container>
     </>
   );
 };
