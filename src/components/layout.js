15867816fc1ffb050594e3ef41b6b81f182e8a1d
Marc Collado <marc@collado.io>
2021-09-19 08:53:32 +0200

    Remove old layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,44 +4,30 @@ import { Link, useStaticQuery, graphql } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
 import 'normalize.css';
 
+import { fluid } from '../utils/fluid';
+import Header from './header';
 import { FlexCenter } from '../styles/containers';
+import GlobalStyles from '../styles/global';
 import Seo from '../utils/seo';
 
 // Styled Components
 
 // TODO: get rid of this container by making outter flex
-const InnerContainer = styled.div`
-  margin: auto 2em;
+const Container = styled.div`
   display: flex;
   flex-flow: column;
-
-  @media (min-width: 576px) {
-    margin: auto 3em;
-  }
-
-  @media (min-width: 768px) {
-    margin: auto 4em;
-  }
-
-  @media (min-width: 1024px) {
-    margin: auto 5em;
-  }
+  margin-block-end: ${fluid(24, 48)};
 `;
 
-const StyledImg = styled(GatsbyImage)`
-  width: 4em;
-  margin: 0em auto 1.5em;
+const StyledAvatar = styled(GatsbyImage)`
+  width: ${fluid(64, 80)};
+  margin: 0px auto ${fluid(16, 40)};
 
   & img {
-    border-radius: 2em;
-  }
-
-  @media (min-width: 576px) {
-    margin: 0em auto 2em;
+    border-radius: ${fluid(32, 40)};
   }
 
   @media (min-width: 768px) {
-    margin: 0em auto 2.5em;
     transition: all 300ms ease-in-out;
 
     &:hover {
@@ -49,17 +35,18 @@ const StyledImg = styled(GatsbyImage)`
         props.pathname === '/' ? 'scale(1, 1)' : 'scale(1.15, 1.15)'};
     }
   }
-  @media (min-width: 1024px) {
-    margin: 0em auto 3em;
-  }
+`;
+
+const CoverImage = styled(GatsbyImage)`
+  margin: ${fluid(16, 20)} 0em ${fluid(8, 12)};
 `;
 
 // Main Components
 
-const Layout = ({ children, article, description, image, pathname, title }) => {
+const Layoutt = ({ children, article, coverImage, md, pathname, seoImage }) => {
   // GraphQL
   const data = useStaticQuery(graphql`
-    query AvatarQuery {
+    query AvatarrQuery {
       file(relativePath: { eq: "logos/marc.png" }) {
         childImageSharp {
           gatsbyImageData(width: 256)
@@ -69,26 +56,37 @@ const Layout = ({ children, article, description, image, pathname, title }) => {
   `);
 
   const avatar = data.file.childImageSharp.gatsbyImageData;
+  const { excerpt, html, title } = md;
 
   return (
     <>
+      <GlobalStyles />
       <Seo
         isArticle={article}
-        pageDescription={description}
-        pageImage={image}
+        pageDescription={excerpt}
+        pageImage={seoImage}
         pageTitle={title}
         pathname={pathname}
       />
-      <InnerContainer>
+      <Container>
         <FlexCenter>
           <Link to="/">
-            <StyledImg image={avatar} alt="Marc Collado" pathname={pathname} />
+            <StyledAvatar
+              image={avatar}
+              alt="Marc Collado"
+              pathname={pathname}
+            />
           </Link>
         </FlexCenter>
+        {!article && <Header title={title} subtitle={excerpt} />}
+        {!article && coverImage && (
+          <CoverImage image={coverImage} title={title} alt={excerpt} />
+        )}
+        {!article && <div dangerouslySetInnerHTML={{ __html: html }} />}
         {children}
-      </InnerContainer>
+      </Container>
     </>
   );
 };
 
-export default Layout;
+export default Layoutt;
