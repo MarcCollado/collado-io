ecbc74a53540bc30015073221059567899c69ed5
Marc Collado <marc@collado.io>
2022-01-21 16:22:15 +0100

    Update Ironhack work page

--- src/content/md/pages/work/ironhack.md
+++ src/content/md/pages/work/ironhack.md
@@ -1,29 +1,38 @@
 ---
-title: 'Ironhack'
 date: '2015-09-01'
-path: '/work/ironhack'
 excerpt: 'Empowering the next generation of digital creators'
-# seo: ''
+path: '/work/ironhack'
+position: 'Campus Manager'
+status: 'active'
+title: 'Ironhack'
+type: 'startup'
 ---
 
-In 2015, after a failed attempt at taking a sabbatical, decided to switch gears and [joined Ironhack as employee #5](https://www.ironhack.com/) to change the way we learn about technology.
+In 2015, [joined Ironhack as employee #5](https://www.ironhack.com/) motivated to change the way we learn about technology.
 
-First, as the Campus Manager in Barcelona — [establishing the city operations](/blog/2015/hi-from-ironhack) from the ground up. Later on, in 2017, in my quest for starting things from scratch, I took the challenge of [setting up the global product team](/blog/2017/back-to-product).
+First, as the Campus Manager in Barcelona — [establishing the city operations](/blog/2015/hi-from-ironhack) from the ground up. Later on, in 2017, in a quest for starting things from scratch, I took the challenge of [setting up the global product team](/blog/2017/back-to-product).
 
-After more than 4 years, and a great deal of nostalgia, [I said goodbye](/blog/2020/thank-you-and-goodbye-ironhack) 👋 to this amazing team. Leaving behind, not only close friends but an organization that I saw grew from bootstrap to almost 200 people across 9 countries.
+Since then, Ironhack has become one of the [most prominent schools](https://techcrunch.com/2021/01/20/miami-based-ironhack-raises-20-million-for-its-coding-bootcamps-as-demand-for-coders-continues) for getting people started in software development, data science, and UX design. Because of this, Ironhack has successfully attracted students who now work at Google, Visa, Twitter, Orange, or Magic Leap.
+
+After more than 4 years, and a great deal of nostalgia, [I said goodbye](/blog/2020/thank-you-and-goodbye-ironhack) 👋 to this amazing team. Leaving behind, not only close friends but an organization that grew from the initial founding team to almost 200 people across 9 countries.
 
 ## ⚡️ Highlights
 
-As the Campus Manager ...
+**As the Campus Manager**
 
-- Set up Barcelona Campus — from 0 to +200 graduates / year
-- Increased overall NPS by +30 points and placement rate over 80%
-- Hired, scaled, and led the campus team — from a one-man show up to 15 people
+- Grew the Barcelona Campus from 0 to +200 graduates / year
+- Increased the overall NPS by +30 points and the placement rate over 80%
+- Hired, scaled, and led the campus team from a one-man show up to 15 people
 - Launched key city partnerships, such as ESADE Business School or [wallapop](https://www.genbeta.com/actualidad/wallapop-y-ironhack-ofreceran-200-000-euros-en-becas-para-formar-a-100-mujeres-en-tecnologia)
 
-As the Product Manager ...
+**As the Product Manager**
 
 - Kickstarted Ironhack's global product team initiative
-- Went from idea to establish the largest team in the organization
-- Led the cross-functional team that delivered the firsts company-wide products
-- After 4+ years and massive growth behind, I left behind the company I'd joined as employee #5
+- Went from idea to establishing the largest team in the entire organization
+- Led the cross-functional team that delivered the firsts foundational company-wide products
+
+## 🗃 More on Ironhack
+
+If you want to learn more about Ironhack, below you'll find additional content that covers its story from a more personal perspective.
+
+![Ironhack Barcelona](../../../img/pages/ironhack-barcelona.jpg 'Ironhack Barcelona')
--- src/pages/work/ironhack.js
+++ src/pages/work/ironhack.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import styled from 'styled-components';
 import { graphql } from 'gatsby';
-import { GatsbyImage } from 'gatsby-plugin-image';
-import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
 
 import Layout from '../../components/layout';
 import { renderPosts, extractMarkdown } from '../../utils/helpers';
-import '../../styles/tabs.css';
-
-const CoverImage = styled(GatsbyImage)`
-  margin: 1em 0em 0.5em;
-`;
 
 const Ironhack = ({ data, location }) => {
   const coverImg = data.coverImg.childImageSharp.gatsbyImageData;
   const seoImg = data.coverImg.childImageSharp.gatsbyImageData.src;
-  const insightsImg = data.insightsImg.childImageSharp.gatsbyImageData;
-  const storiesImg = data.storiesImg.childImageSharp.gatsbyImageData;
   const md = extractMarkdown(data.md.edges);
   const posts = data.posts.edges;
 
@@ -28,57 +18,7 @@ const Ironhack = ({ data, location }) => {
       pathname={location.pathname}
       seoImage={seoImg}
     >
-      <Tabs>
-        <TabList>
-          <Tab>
-            <p>Insights</p>
-          </Tab>
-          <Tab>
-            <p>Stories</p>
-          </Tab>
-        </TabList>
-
-        <TabPanel>
-          <p>
-            Education is changing for the better. At Ironhack I had a unique
-            opportunity to be part and lead this revolution; using technology to
-            create the tools for our students to boost their careers and become
-            digital creators themselves.
-          </p>
-          <CoverImage
-            image={insightsImg}
-            title="Ironhack insights"
-            alt="Ironhack is changing education for the better."
-          />
-          <p>
-            Thousands of graduates across eleven locations have taught me a lot
-            about education and how we learn. Here is a recollection of posts
-            around educational products and methodologies. What has worked, what
-            has not, and the lessons we have learned along the way.
-          </p>
-          {renderPosts(posts, 'idea')}
-        </TabPanel>
-        <TabPanel>
-          <p>
-            Back in 2015, when I joined Ironhack, it looked nothing like today.
-            We were barely ten of us and the idea of changing people lives in
-            just two months sounded more of a wild dream rather than an
-            attainable reality.
-          </p>
-          <CoverImage
-            image={storiesImg}
-            title="Ironhack stories"
-            alt="Recollection of posts that cover my story at Ironhack from a more confidential, idiosyncratic perspective."
-          />
-          <p>
-            After more than four years, I have accumulated thousands of stories,
-            from being a student myself to scaling campuses operations. Here is
-            a recollection of posts that cover my story at Ironhack from a more
-            confidential, idiosyncratic perspective.
-          </p>
-          {renderPosts(posts, 'memoir')}
-        </TabPanel>
-      </Tabs>
+      {renderPosts(posts)}
     </Layout>
   );
 };
@@ -103,16 +43,6 @@ export const query = graphql`
       ...allPosts
     }
     coverImg: file(relativePath: { eq: "pages/ironhack-cover.jpg" }) {
-      childImageSharp {
-        gatsbyImageData(width: 1024)
-      }
-    }
-    insightsImg: file(relativePath: { eq: "pages/ironhack-insights.jpg" }) {
-      childImageSharp {
-        gatsbyImageData(width: 1024)
-      }
-    }
-    storiesImg: file(relativePath: { eq: "pages/ironhack-barcelona.jpg" }) {
       childImageSharp {
         gatsbyImageData(width: 1024)
       }
