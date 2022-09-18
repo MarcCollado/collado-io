03811b1189a3ef6b5cc3caac9e8d28f68240688d
Marc Collado <marc@collado.io>
2022-09-18 09:11:24 +0200

    Improve documentation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -21,33 +21,30 @@ export const workmaps = {
 
 /**
  * Extracts page information from the corresponding markdown file
- * @param {array} edges - pageMarkdown from GraphQL query results
- * @returns {object} - page information
+ * @param {array} edges -> markdown file from GraphQL query results
+ * @returns {object} page information
  */
 export function extractMarkdown(edges) {
   if (typeof edges !== 'object' || edges.length !== 1) {
     throw new Error('Expected an array with one item.');
   }
-
-  const md = {
+  const markdownFile = {
     date: edges[0].node.frontmatter?.date,
     excerpt: edges[0].node.frontmatter?.excerpt,
     path: edges[0].node.frontmatter?.path,
-    // seo: edges[0].node.frontmatter?.seo,
     title: edges[0].node.frontmatter?.title,
     html: edges[0].node.html,
     id: edges[0].node.id,
   };
-
-  return md;
+  return markdownFile;
 }
 
 /**
  * Generates and renders a list of posts
  * @generator
- * @param {array} edges - allPosts from GraphQL query results
- * @param {string} filterTag — render exclusively posts with that tag
- * @returns {array} - list of cards w/o the html
+ * @param {array} edges -> allPosts from GraphQL query results
+ * @param {string} filterTag -> render exclusively posts with that tag
+ * @returns {array} -> list of cards w/o the html
  */
 export function renderPosts(edges, filterTag = '') {
   if (typeof edges !== 'object') {
