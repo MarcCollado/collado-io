ee8c962bd83dec7ee0d7ccd26f17e36187b05546
Marc Collado <marc@collado.io>
2022-09-18 15:35:07 +0200

    Comment out SEO

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Link } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
 
-import Seo from '../utils/seo';
+// import SEO from '';
 
 const Layout = ({ children, location }) => {
   const rootPath = `${__PATH_PREFIX__}/`;
