10840914be7618c35563909ea81409afa4cf0d49
Marc Collado <marc@collado.io>
2022-09-18 18:44:57 +0200

    Refactor code

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,73 +1,24 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import styled from 'styled-components';
 
-import { fluid } from '../utils/fluid';
-import Layout from './layout';
-import PostMeta from './postMeta';
+import Layout from '../components/layout';
+// import SEO from '../components/seo';
 
-// Styled Components
-
-const PostTitle = styled.h1`
-  font-size: 1.5em;
-
-  @media (min-width: 768px) {
-    font-size: 1.75em;
-  }
-`;
-
-const PostArticle = styled.div`
-  *:first-child {
-    margin-block-start: 0.5em;
-  }
-
-  hr {
-    margin: ${fluid(32, 40)} 0px;
-  }
-
-  div > hr {
-    display: none;
-  }
-
-  div > ol > li > a:hover,
-  div > ol > li > a:hover {
-    background: none;
-    color: var(--bianchiGreen);
-  }
-
-  // Center GIFs
-  p > img {
-    display: block;
-    margin: 0px auto;
-  }
-`;
-
-// Main Components
-
-const PostPage = ({ data, pageContext }) => {
-  // const { frontmatter, html, id } = data.markdownRemark;
+const BlogPost = ({ data, pageContext }) => {
   const { frontmatter, html } = data.markdownRemark;
-  // const { date, excerpt, featured, path, source, tags, title } = frontmatter;
-  const { date, path, tags, title } = frontmatter;
-  const next = pageContext.next.frontmatter.path;
-  const prev = pageContext.prev.frontmatter.path;
+  const { date, excerpt, path, tags, title } = frontmatter;
+  // const next = pageContext.next.frontmatter.path;
+  // const prev = pageContext.prev.frontmatter.path;
 
   return (
-    <Layout
-      article={true}
-      coverImage={false}
-      md={frontmatter}
-      pathname={path}
-      seoImage={false}
-    >
-      <PostTitle>{title}</PostTitle>
-      <PostArticle dangerouslySetInnerHTML={{ __html: html }} />
-      <PostMeta
-        date={date}
-        next={next === path ? null : next}
-        prev={prev === path ? null : prev}
-        tags={tags}
-      />
+    <Layout location={''}>
+      <>
+        {/* <SEO title={siteTitle}></SEO> */}
+        <h1>{title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+        <p>{date}</p>
+        <p>{tags}</p>
+      </>
     </Layout>
   );
 };
@@ -75,21 +26,21 @@ const PostPage = ({ data, pageContext }) => {
 // GraphQL
 
 export const query = graphql`
-  query PostPageQuery($path: String!) {
+  query BlogPostQuery($path: String!) {
     markdownRemark(frontmatter: { path: { eq: $path } }) {
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
         excerpt
-        featured
+        # featured
         path
-        source
+        # source
         tags
         title
       }
       html
-      id
+      # id
     }
   }
 `;
 
-export default PostPage;
+export default BlogPost;
--- src/templates/tag-page.js
+++ src/templates/tag-page.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { graphql } from 'gatsby';
+import * as React from 'react';
+import { Link, graphql } from 'gatsby';
 
-import Layout from './layout';
-import { renderPosts } from '../utils/helpers';
+import Layout from '../components/layout';
+// import SEO from '../components/seo';
 
-const TagPage = ({ data, location, pageContext }) => {
+const Tag = ({ data, location, pageContext }) => {
+  const siteTitle = data.site.siteMetadata?.title || `Tag`;
   const { totalCount } = data.allMarkdownRemark;
   const posts = data.allMarkdownRemark.edges;
   const { tag } = pageContext;
@@ -18,14 +19,40 @@ const TagPage = ({ data, location, pageContext }) => {
   };
 
   return (
-    <Layout
-      article={false}
-      coverImage={false}
-      md={md}
-      pathname={location.pathname}
-      seoImage={false}
-    >
-      {renderPosts(posts)}
+    <Layout location={location}>
+      {/* <SEO title={siteTitle} /> */}
+      <ol style={{ listStyle: `none` }}>
+        {posts.map((post) => {
+          const title = post.node.frontmatter.title;
+          return (
+            <li key={post.node.id}>
+              <article
+                className="post-list-item"
+                // itemScope
+                // itemType="http://schema.org/Article"
+              >
+                <header>
+                  <h2>
+                    <Link to={post.node.frontmatter.path} itemProp="url">
+                      <span itemProp="headline">{title}</span>
+                    </Link>
+                  </h2>
+                  <small>{post.node.frontmatter.date}</small>
+                </header>
+                {/* Some featured posts may feature its description inline
+                  <section>
+                   <p
+                     dangerouslySetInnerHTML={{
+                       __html: post.frontmatter.excerpt || post.excerpt,
+                     }}
+                     itemProp="description"
+                   />
+                 </section> */}
+              </article>
+            </li>
+          );
+        })}
+      </ol>
     </Layout>
   );
 };
@@ -34,6 +61,11 @@ const TagPage = ({ data, location, pageContext }) => {
 
 export const query = graphql`
   query tagPageQuery($tag: String) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
     allMarkdownRemark(
       filter: {
         fileAbsolutePath: { regex: "/src/content/md/posts/" }
@@ -47,4 +79,4 @@ export const query = graphql`
   }
 `;
 
-export default TagPage;
+export default Tag;
