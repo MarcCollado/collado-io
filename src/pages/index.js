698209ee4b5b861953c24ca208005e20a308f88d
Marc Collado <marc@collado.io>
2022-09-18 09:11:39 +0200

    Update with new code structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,35 @@
-import React from 'react';
+import * as React from 'react';
 import { graphql } from 'gatsby';
 
 import Layout from '../components/layout';
+// import SEO from '../components/seo';
 import { extractMarkdown } from '../utils/helpers';
 
-const HomePage = ({ data, location }) => {
-  const md = extractMarkdown(data.md.edges);
-
+const Home = ({ data, location }) => {
+  const siteTitle =
+    data.site.siteMetadata?.title || `Marc Collado's personal website`;
+  const markdownFile = extractMarkdown(data.allMarkdownRemark.edges);
   return (
-    <Layout
-      article={false}
-      coverImage={false}
-      md={md}
-      pathname={location.pathname}
-      seoImage={false}
-    />
+    <Layout location={location}>
+      {/* <SEO title={siteTitle}></SEO> */}
+      <p className="global-header">Hola! I'm</p>
+      <h1 className="main-heading">Marc Collado</h1>
+      <div>dangerouslySetInnerHTML={{ __html: markdownFile.html }}</div>
+    </Layout>
   );
 };
 
 export const query = graphql`
   {
-    md: allMarkdownRemark(
-      filter: { fileAbsolutePath: { regex: "/src/content/md/pages/index.md/" } }
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(
+      filter: {
+        fileAbsolutePath: { regex: "/src/media/markdown/pages/index.md/" }
+      }
       limit: 1
     ) {
       ...pageMarkdown
@@ -29,4 +37,4 @@ export const query = graphql`
   }
 `;
 
-export default HomePage;
+export default Home;
