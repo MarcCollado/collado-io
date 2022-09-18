8d7457c9d727ca7f1c4b88ba879b7357936dee8b
Marc Collado <marc@collado.io>
2022-09-18 09:11:09 +0200

    Remove SEO from frontmatter

--- src/utils/queries.js
+++ src/utils/queries.js
@@ -40,7 +40,6 @@ export const PageMarkdownQuery = graphql`
           date(formatString: "MMMM DD, YYYY")
           excerpt
           path
-          # seo
           title
         }
         html
