bd34540a8d5aa1454a1cd532cbd30ba0a1afecd3
Marc Collado <marc@collado.io>
2022-09-22 10:51:38 +0200

    Render footer

--- src/templates/post-page.js
+++ src/templates/post-page.js
@@ -5,27 +5,29 @@ import Layout from '../components/layout';
 // import SEO from '../components/seo';
 import { toTitleCase } from '../utils/helpers';
 
-const Post = ({ data, pageContext }) => {
+const Post = ({ data, location, pageContext }) => {
   const { frontmatter, html } = data.markdownRemark;
   const { date, excerpt, path, tags, title } = frontmatter;
   // const next = pageContext.next.frontmatter.path;
   // const prev = pageContext.prev.frontmatter.path;
   return (
-    <Layout location={''}>
+    <Layout location={location}>
       <article>
         {/* <SEO title={siteTitle}></SEO> */}
         <h1>{toTitleCase(title)}</h1>
         <div dangerouslySetInnerHTML={{ __html: html }} />
-        <small>{date}</small>
-        <div className="tag-box">
-          {tags.map((tag) => {
-            const tagPath = `/tags/${tag}`;
-            return (
-              <small>
-                <Link to={tagPath}>{`#${tag}`}</Link>
-              </small>
-            );
-          })}
+        <div style={{ textAlign: 'center' }}>
+          <small>{`First published on ${date}`}</small>
+          <div className="tag-box">
+            {tags.map((tag) => {
+              const tagPath = `/tags/${tag}`;
+              return (
+                <small>
+                  <Link to={tagPath}>{`#${tag}`}</Link>
+                </small>
+              );
+            })}
+          </div>
         </div>
       </article>
     </Layout>
