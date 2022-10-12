b6ee84df76ae94d3f02a254842a74e596ac76aab
Marc Collado <marc@collado.io>
2022-10-12 13:29:47 +0200

    Add placeholder for Layout wrapper

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,12 @@
+// import React from 'react';
+
 import './src/styles/normalize.css';
 import './src/styles/style.css';
 import 'prismjs/themes/prism.css';
+
+// TODO: wrap all pages with Layout instead of importing 1by1
+// import Layout from './src/components/layout';
+
+// export function wrapPageElement({ element, props }) {
+//   return <Layout {...props}>{element}</Layout>;
+// }
