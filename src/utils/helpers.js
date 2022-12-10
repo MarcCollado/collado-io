175c0da935b2dc86144edc2bcc47d63b525b0e81
Marc Collado <marc@collado.io>
2022-12-10 20:23:08 +0100

    Create custom filter to titleCase words

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -18,7 +18,51 @@ export function extractMarkdown(edges) {
 }
 
 export function toTitleCase(str) {
+  const lowerCaseFilter = [
+    'a',
+    'an',
+    'and',
+    'as',
+    'at',
+    'but',
+    'by',
+    'for',
+    'if',
+    'in',
+    'of',
+    'on',
+    'or',
+    'the',
+    'to',
+    'vs.',
+    // Català
+    'de',
+    'des',
+    // Ad-hoc
+    'collado.io',
+    'ebay',
+    'iomando',
+    'v2',
+  ];
+  const upperCaseFilter = ['AI', 'API', 'II', 'III', 'IV', 'V'];
+  let isFirstWord = true;
   return str.replace(/\w\S*/g, function (txt) {
+    // Ignore the first word
+    if (isFirstWord) {
+      isFirstWord = false;
+      return txt;
+    }
+
+    // Return these words lowercase
+    if (lowerCaseFilter.includes(txt.toLowerCase())) {
+      return txt.toLowerCase();
+    }
+
+    // Return these words upperCase
+    if (upperCaseFilter.includes(txt.toUpperCase())) {
+      return txt.toUpperCase();
+    }
+
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
 }
