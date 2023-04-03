cb6bb93ee35188a1d75b6ff4087025607fca0a78
Marc Collado <marc@collado.io>
2023-04-03 20:15:24 +0200

    Source external RSS feeds

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,58 +2,24 @@ import * as React from 'react';
 import { Link, graphql } from 'gatsby';
 
 import Layout from '../components/layout';
-import { toTitleCase } from '../utils/helpers';
+import { createFeed } from '../utils/helpers';
 
 const Blog = ({ data, location }) => {
-  const posts = data.allMarkdownRemark.edges;
   const seoData = {
     pageDescription: `All blog posts.`,
     pageTitle: ``,
   };
+
   return (
     <Layout location={location} seoData={seoData}>
-      <ol style={{ listStyle: `none` }}>
-        {posts.map((post) => {
-          const { date, excerpt, featured, title, path } =
-            post.node.frontmatter;
-          const isFeatured = featured;
-          return (
-            <li key={post.node.id}>
-              <article
-                className="post-list-item"
-                // itemScope
-                // itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={path} itemProp="url">
-                      <span itemProp="title">{toTitleCase(title)}</span>
-                    </Link>
-                  </h2>
-                  <small itemProp="date">{date}</small>
-                </header>
-                {isFeatured && (
-                  <section>
-                    <small
-                      dangerouslySetInnerHTML={{
-                        __html: excerpt || post.excerpt,
-                      }}
-                      itemProp="description"
-                    />
-                  </section>
-                )}
-              </article>
-            </li>
-          );
-        })}
-      </ol>
+      <ol style={{ listStyle: `none` }}>{createFeed(data)}</ol>
     </Layout>
   );
 };
 
 export const blogQuery = graphql`
   query {
-    allMarkdownRemark(
+    posts: allMarkdownRemark(
       filter: {
         fileAbsolutePath: { regex: "/src/media/markdown/posts/" }
         frontmatter: { tags: { nin: ["drafts", "now"] } }
@@ -61,6 +27,12 @@ export const blogQuery = graphql`
       sort: { frontmatter: { date: DESC } }
     ) {
       ...allPosts
+    }
+    safareigEpisodes: allFeedSafareig(sort: { isoDate: DESC }) {
+      ...allSafareigEpisodes
+    }
+    fatEpisodes: allFeedFocATerra(sort: { isoDate: DESC }) {
+      ...allFocATerraEpisodes
     }
   }
 `;
