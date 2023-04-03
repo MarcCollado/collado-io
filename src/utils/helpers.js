3b88b4cd1dd26b86fb49be9ee7c2b514f91c0a83
Marc Collado <marc@collado.io>
2023-04-03 20:16:14 +0200

    Create function to generate feed

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
 /**
  * Extracts page information from the corresponding markdown file
  * @param {array} edges with one object inside
@@ -65,4 +68,76 @@ export function toTitleCase(str) {
 
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
+}
+
+export function createFeed(data) {
+  // Append "project" property to each collection
+  data.posts.edges.map((e) => (e.node['project'] = 'collado.io'));
+  data.safareigEpisodes.edges.map((e) => (e.node['project'] = 'safareig.fm'));
+  data.fatEpisodes.edges.map((e) => (e.node['project'] = 'focaterra.com'));
+
+  // Create unified feed
+  const blogPosts = data.posts.edges;
+  const podcastEpisodes = [
+    ...data.safareigEpisodes.edges,
+    ...data.fatEpisodes.edges,
+  ];
+  const feed = [...blogPosts, ...podcastEpisodes];
+
+  // Sort unified feed by date
+  return feed
+    .sort((a, b) => {
+      const dateA = new Date(a.node.frontmatter?.date || a.node.isoDate);
+      const dateB = new Date(b.node.frontmatter?.date || b.node.isoDate);
+      return dateB - dateA;
+    })
+    .map((e) => {
+      if (e.node.frontmatter?.date) {
+        const { date, excerpt, featured, title, path } = e.node.frontmatter;
+        return (
+          <li key={e.node.id}>
+            <article
+              className="post-list-item"
+              // itemScope
+              // itemType="http://schema.org/Article"
+            >
+              <header>
+                <h2>
+                  <Link to={path} itemProp="url">
+                    <span itemProp="title">{toTitleCase(title)}</span>
+                  </Link>
+                </h2>
+                <small itemProp="date">{date}</small>
+              </header>
+              {featured && (
+                <section>
+                  <small
+                    dangerouslySetInnerHTML={{
+                      __html: excerpt || e.excerpt,
+                    }}
+                    itemProp="description"
+                  />
+                </section>
+              )}
+            </article>
+          </li>
+        );
+      } else {
+        const { link, id, itunes, isoDate: date, title } = e.node;
+        return (
+          <li key={id} className="post-list-item">
+            <header>
+              <h2>
+                <a href={link} itemProp="url">
+                  <span itemProp="title">
+                    {`${itunes.episode}: ${title} ->`}
+                  </span>
+                </a>
+              </h2>
+              <small itemProp="date">{date}</small>
+            </header>
+          </li>
+        );
+      }
+    });
 }
