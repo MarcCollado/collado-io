4f2a152c364e61184a4122a4a4409f25381b4478
Marc Collado <marc@collado.io>
2024-01-22 16:19:23 +0100

    Use Gatsby Head API

--- src/pages/podcast.js
+++ src/pages/podcast.js
@@ -2,20 +2,14 @@ import * as React from 'react';
 import { graphql } from 'gatsby';
 
 import Layout from '../components/layout';
+import Seo from '../components/seo';
 import { podcastFeedGenerator } from '../utils/helpers';
 
-const Podcast = ({ data, location }) => {
-  const seoData = {
-    pageDescription: `Podcast episodes`,
-    pageTitle: `Podcast`,
-  };
-
-  return (
-    <Layout location={location} seoData={seoData}>
-      <ol style={{ listStyle: `none` }}>{podcastFeedGenerator(data)}</ol>
-    </Layout>
-  );
-};
+const Podcast = ({ data, location }) => (
+  <Layout location={location}>
+    <ol style={{ listStyle: `none` }}>{podcastFeedGenerator(data)}</ol>
+  </Layout>
+);
 
 export const episodesQuery = graphql`
   query {
@@ -30,5 +24,13 @@ export const episodesQuery = graphql`
     }
   }
 `;
+
+export const Head = ({ location }) => (
+  <Seo
+    pageTitle="Podcast — Marc Collado"
+    pageDescription="Marc's podcast episodes"
+    location={location}
+  />
+);
 
 export default Podcast;
