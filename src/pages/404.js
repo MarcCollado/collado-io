107e7b3a92a4bd9820e0d7cae7c0c0b5d20d220c
Marc Collado <marc@collado.io>
2024-01-22 16:17:59 +0100

    Use Gatsby Head API

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,16 +2,13 @@ import * as React from 'react';
 import { graphql } from 'gatsby';
 
 import Layout from '../components/layout';
-// import SEO from '../components/seo';
+import Seo from '../components/seo';
 import { extractMarkdown } from '../utils/helpers';
 
 const NotFound = ({ data, location }) => {
-  // const siteTitle = data.site.siteMetadata?.title;
-  // const authorName = data.site.siteMetadata?.author?.name;
   const markdownData = extractMarkdown(data.allMarkdownRemark.edges);
   return (
     <Layout location={location}>
-      {/* <SEO title={siteTitle}></SEO> */}
       <p className="heading-companion">{markdownData.title}</p>
       <h1 className="heading">{markdownData.excerpt}</h1>
       <div dangerouslySetInnerHTML={{ __html: markdownData.html }} />
@@ -21,14 +18,6 @@ const NotFound = ({ data, location }) => {
 
 export const query = graphql`
   {
-    site {
-      siteMetadata {
-        author {
-          name
-        }
-        title
-      }
-    }
     allMarkdownRemark(
       filter: { fileAbsolutePath: { regex: "/src/media/pages/404.md/" } }
       limit: 1
@@ -37,5 +26,13 @@ export const query = graphql`
     }
   }
 `;
+
+export const Head = ({ location }) => (
+  <Seo
+    pageTitle="404 — Marc Collado"
+    pageDescription="Page not found"
+    location={location}
+  />
+);
 
 export default NotFound;
