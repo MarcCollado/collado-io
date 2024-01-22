127691a66a674d7449323736182c002f1182bbd6
Marc Collado <marc@collado.io>
2024-01-22 13:20:03 +0100

    Drop Helment component

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,11 +1,10 @@
-import * as React from 'react';
+import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
-import { Helmet } from 'react-helmet';
 
 import favicon from '../../static/favicon.ico';
 import seoImage from '../../static/marc-avatar.png';
 
-const Seo = ({ pageDescription, pageTitle, pathname }) => {
+const SEO = ({ pageTitle, pageDescription, location, children }) => {
   const { site } = useStaticQuery(graphql`
     query {
       site {
@@ -19,9 +18,6 @@ const Seo = ({ pageDescription, pageTitle, pathname }) => {
           siteUrl
           social {
             email
-            github
-            linkedin
-            strava
             twitter
           }
         }
@@ -29,41 +25,51 @@ const Seo = ({ pageDescription, pageTitle, pathname }) => {
     }
   `);
 
+  const { pathname } = location;
   const author = site.siteMetadata?.author.name;
-  const authorEmail = site.siteMetadata?.social.email;
-  const description = pageDescription || site.siteMetadata?.defaultDescription;
   const image = seoImage;
   const language = site.siteMetadata?.siteLanguage;
   const title = pageTitle || site.siteMetadata?.defaultTitle;
+  const description = pageDescription || site.siteMetadata?.defaultDescription;
   const url = pathname
     ? `${site.siteMetadata?.siteUrl}${pathname}`
     : site.siteMetadata?.siteUrl;
+  const social = site.siteMetadata?.social;
 
   return (
-    <Helmet
-      title={title}
-      meta={[
-        // HTML TAGS — https://gist.github.com/whitingx/3840905
-        { name: `description`, content: description },
-        { name: `image`, content: image },
-        { name: `url`, content: url },
-        { name: `author`, content: author },
-        { name: `language`, content: language },
+    <>
+      <title>{title}</title>
 
-        // OG TAGS — https://opengraphprotocol.org
-        { property: `og:title`, content: title },
-        { property: `og:description`, content: description },
-        { property: `og:image`, content: image },
-        { property: `og:url`, content: url },
-        { property: `og:email`, content: authorEmail },
+      {/* HTML TAGS */}
+      <meta name="description" content={description} />
+      <meta name="image" content={image} />
+      <meta name="url" content={url} />
+      <meta name="author" content={author} />
+      <meta name="language" content={language} />
 
-        // COLORED TABS
-        { name: `theme-color`, content: `#19e597` },
-      ]}
-    >
+      {/* OG TAGS */}
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={description} />
+      <meta name="og:image" content={image} />
+      <meta name="og:url" content={url} />
+
+      {/* TWITTER TAGS */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
+      <meta name="twitter:url" content={url} />
+      <meta name="twitter:creator" content={social.twitter} />
+
+      {/* FAVICON */}
       <link rel="icon" type="image/x-icon" href={favicon} />
-    </Helmet>
+
+      {/* COLOR TABS */}
+      <meta name="theme-color" content="#19e597" />
+
+      {children}
+    </>
   );
 };
 
-export default Seo;
+export default SEO;
